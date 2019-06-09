577d16e20c71f34808d5a1615461ae07ff14cc57
Justin D <justin@baal-software.com>
2019-06-09 16:28:48 -0600

    Adds ring-json to handle json responses

--- src/cheshire_cat/handler.clj
+++ src/cheshire_cat/handler.clj
@@ -2,11 +2,19 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [cheshire.core :as json]))
+            [ring.middleware.json :as json]
+            [ring.util.response :as rr]))
 
 (defroutes app-routes
   (GET "/" [] "<h1>Welcome</h1>")
+  (GET "/cheshire-cat" []
+        (rr/response {:name "Chesire Cat"
+                :status :grinning
+                :folk :yessir}))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      (json/wrap-json-response)
+      (wrap-defaults site-defaults)
+      ))
