772a51e2d12c6bdab2e51109b43cbef9b51793d7
Justin D <justin@baal-software.com>
2019-06-09 19:07:37 -0600

    Adds support for clojurescript

--- project.clj
+++ project.clj
@@ -5,9 +5,18 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
-                 [ring/ring-json "0.4.0"]]
-  :plugins [[lein-ring "0.12.5"]]
+                 [ring/ring-json "0.4.0"]
+                 [org.clojure/clojurescript "1.10.516"]]
+  :plugins [[lein-ring "0.12.5"]
+            [lein-cljsbuild "1.1.7"]]
   :ring {:handler cheshire-cat.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}})
+                        [ring/ring-mock "0.3.2"]]}}
+  :cljsbuild
+  {:builds
+   [{:compiler
+     {:output-to "resources/public/main.js",
+      :optimizations :whitespace,
+      :pretty-print true},
+     :source-paths ["src-cljs"]}]})
---
+++ src-cljs/core.cljs
@@ -0,0 +1,6 @@
+(ns cheshire-cat.core
+  (:require [clojure.browser.repl :as repl]))
+
+
+(defn ^:export init []
+  (repl/connect "http://localhost:9000/repl"))
