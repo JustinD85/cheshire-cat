8225ddc07e8c8ee169b2562e80daad6f5c11e863
Justin D <justin@baal-software.com>
2019-06-09 19:41:41 -0600

    Adds functionality to grab info from a get request

--- src-cljs/core.cljs
+++ src-cljs/core.cljs
@@ -1,6 +1,11 @@
 (ns cheshire-cat.core
-  (:require [clojure.browser.repl :as repl]))
-
+  (:require [clojure.browser.repl :as repl]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (defn ^:export init []
-  (repl/connect "http://localhost:9000/repl"))
+  (repl/connect "http://localhost:9000/repl")
+  (go
+    (let [response (<! (http/get "/cheshire-cat"))]
+      (js/alert (:body response)))))
